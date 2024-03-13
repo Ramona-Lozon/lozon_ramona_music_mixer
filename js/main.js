54045ff245f8fc8e4d911ac5e6932de3ad7ae78a
Ramona <144703303+Ramona-Lozon@users.noreply.github.com>
2024-03-13 17:27:05 -0400

    added functionality to several buttons

--- js/main.js
+++ js/main.js
@@ -1,17 +1,21 @@
 // Query Selectors!!! :D
-
-
 let soundIcons = document.querySelectorAll('.sound_icon'),
     dropZone = document.querySelectorAll('.drop_zone'),
     draggedIcon;
 const resetButton = document.getElementById('resetButton');
 const iconPanel = document.getElementById('icon_panel');
+const playButton = document.getElementById('playButton');
+const pauseButton = document.getElementById('pauseButton');
+const rewindButton = document.getElementById('rewindButton');
+const volSlider = document.getElementById('volumeControl');
+
+// Array to store references to all playing audio elements
+const dropZoneAudioList = []; 
 
 // Drag and Drop Functions !!!! :D
 
-
 // Drag Function
-function handleStartDrag() { 
+function handleStartDrag() {
     console.log('started dragging this icon:', this);
     draggedIcon = this;
 }
@@ -19,13 +23,13 @@ function handleStartDrag() {
 // Drag over Drop Zone Function
 function handleDragOver(e) {
     e.preventDefault();
-    console.log('dragged over me'); 
+    console.log('dragged over drop zone');
 }
 
 // Drop Function!!!
-function handleDrop(e) { 
+function handleDrop(e) {
     e.preventDefault();
-    console.log('dropped something on me');
+    console.log('dropped something in drop zone');
     if (!this.querySelector('img')) {
         const clonedIcon = draggedIcon.cloneNode(true);
         draggedIcon.parentNode.removeChild(draggedIcon);
@@ -39,38 +43,19 @@ function handleDrop(e) {
 
 // Reset Function!!!
 function resetImages() {
-    
-// Remove all images from drop zone
+    // Remove all images from drop zone
     dropZone.forEach(zone => {
         zone.innerHTML = '';
     });
 
-// Append all images back to icon panel
+    // Append all images back to icon panel
     soundIcons.forEach(icon => {
         iconPanel.appendChild(icon);
     });
+
+    // Reset the dropZoneAudioList array
+    dropZoneAudioList.length = 0;
 }
-
-
-// audio Functions!!! :D
-
-
-// Play Audio Function
-// function playAudio() {
-//     const droppedImage = document.querySelector('.drop_zone img');
-//     if (droppedImage) {
-//         const trackRef = droppedImage.getAttribute('data-trackref');
-//         if (trackRef) {
-//             const audioSrc = `assets/sounds/${trackRef}`;
-//             dropZoneAudio = new Audio(audioSrc);
-//             dropZoneAudio.play();
-//         } else {
-//             console.error('Data-trackref attribute is missing or empty.');
-//         }
-//     } else {
-//         console.error('No image dropped in the drop zone.');
-//     }
-// }
 
 // Play Audio Function
 function playAudio() {
@@ -82,6 +67,7 @@ function playAudio() {
                 const audioSrc = `assets/sounds/${trackRef}`;
                 const audio = new Audio(audioSrc);
                 audio.play();
+                dropZoneAudioList.push(audio); // Store reference to the playing audio
             } else {
                 console.error('Data-trackref attribute is missing or empty.');
             }
@@ -91,24 +77,30 @@ function playAudio() {
     });
 }
 
-
-// pause Audio Function!!!
+// Pause Audio Function!!!
 function pauseAudio() {
-    dropZoneAudio.pause();
+    dropZoneAudioList.forEach(audio => {
+        audio.pause();
+    });
 }
-
 
 // Rewind Audio Function!!!
 function restartAudio() {
+    dropZoneAudioList.forEach(audio => {
+        audio.currentTime = 0;
+        audio.play();
+    });
 }
 
 // Volume Slider Function!!!
 function setVolume() {
+    console.log(this.value);
+    dropZoneAudioList.forEach(audio => {
+        audio.volume = this.value / 100;
+    });
 }
 
-
-// event listeners!!! :D
-
+// Event listeners!!! :D
 
 // drag item event listener
 soundIcons.forEach(icon => icon.addEventListener('dragstart', handleStartDrag));
